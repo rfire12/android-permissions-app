e9d6d5712445cc9608a748dd763f487b8835d254
Raul Paulino <20461484+rfire12@users.noreply.github.com>
2020-10-11 21:07:10 -0400

    Block switch if permission is already granted

--- app/src/main/java/com/example/android_permissions_app/MainActivity.java
+++ app/src/main/java/com/example/android_permissions_app/MainActivity.java
@@ -2,15 +2,20 @@ package com.example.android_permissions_app;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+
 
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.Manifest;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.Switch;
+
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,6 +33,9 @@ public class MainActivity extends AppCompatActivity {
     private Switch cameraSw;
     private Switch phoneSw;
     private Switch contactsSw;
+    private Button cancelBtn;
+    private Button continueBtn;
+    private ConstraintLayout settingsLayout;
 
 
 
@@ -37,14 +45,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        storageSw = findViewById(R.id.storageSw);
-        locationSw = findViewById(R.id.locationSw);
-        cameraSw = findViewById(R.id.cameraSw);
-        phoneSw = findViewById(R.id.phoneSw);
-        contactsSw = findViewById(R.id.contactsSw);
-
-        Button cancelBtn = findViewById(R.id.cancelBtn);
-        Button continueBtn = findViewById(R.id.continueBtn);
+        initialize();
+        checkGrantedPermissions();
+        onClickSwitchButton();
 
         // Close APP
         cancelBtn.setOnClickListener(new View.OnClickListener() {
@@ -78,6 +81,58 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+    }
+
+    public void initialize() {
+        // Switches
+        storageSw = findViewById(R.id.storageSw);
+        locationSw = findViewById(R.id.locationSw);
+        cameraSw = findViewById(R.id.cameraSw);
+        phoneSw = findViewById(R.id.phoneSw);
+        contactsSw = findViewById(R.id.contactsSw);
+        // Buttons
+        cancelBtn = findViewById(R.id.cancelBtn);
+        continueBtn = findViewById(R.id.continueBtn);
+        // Layouts
+        settingsLayout = findViewById(R.id.settingsLayout);
+    }
+
+    public void checkGrantedPermissions() {
+        if (isPermissionGranted(READ_EXTERNAL_STORAGE))
+            storageSw.setChecked(true);
+        if (isPermissionGranted(ACCESS_FINE_LOCATION))
+            locationSw.setChecked(true);
+        if (isPermissionGranted(CAMERA))
+            cameraSw.setChecked(true);
+        if (isPermissionGranted(CALL_PHONE))
+            phoneSw.setChecked(true);
+        if (isPermissionGranted(READ_CONTACTS))
+            contactsSw.setChecked(true);
+    }
+
+    public void onClickSwitchButton() {
+        storageSw.setOnCheckedChangeListener(switchButtonListenerCallback(storageSw, READ_EXTERNAL_STORAGE));
+        locationSw.setOnCheckedChangeListener(switchButtonListenerCallback(locationSw, ACCESS_FINE_LOCATION));
+        cameraSw.setOnCheckedChangeListener(switchButtonListenerCallback(cameraSw, CAMERA));
+        phoneSw.setOnCheckedChangeListener(switchButtonListenerCallback(phoneSw, CALL_PHONE));
+        contactsSw.setOnCheckedChangeListener(switchButtonListenerCallback(contactsSw, READ_CONTACTS));
+    }
+
+    public CompoundButton.OnCheckedChangeListener switchButtonListenerCallback(final Switch switchBtn, final String permission) {
+        return new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isPermissionGranted(permission)) {
+                    switchBtn.setChecked(true);
+                    showSnackbar("Permission Granted");
+                }
+            }
+        };
+    }
+
+    public void showSnackbar(String text) {
+        Snackbar snackbar = Snackbar.make(settingsLayout, text, Snackbar.LENGTH_SHORT);
+        snackbar.show();
     }
 
     public Boolean isPermissionGranted(String permission) {
